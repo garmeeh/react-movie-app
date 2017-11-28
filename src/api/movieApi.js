07222c81484816bb60f7a94309716cb2353e2cb1
Gary Meehan <garymeehan@outlook.com>
2017-11-28 19:13:02 +0000

    Switch to Async/Await

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -1,16 +1,24 @@
 const API_KEY = '981f13a0';
 const MOVIE_API_BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
 
-const getMovies = searchTerm => {
-  return fetch(`${MOVIE_API_BASE_URL}&s=${searchTerm}`)
-    .then(response => response.json())
-    .catch(error => error);
+const getMovies = async searchTerm => {
+  try {
+    const request = await fetch(`${MOVIE_API_BASE_URL}&s=${searchTerm}`);
+    const json = await request.json();
+    return json;
+  } catch (error) {
+    console.error('API:getMovies', error);
+  }
 };
 
-const getMovie = id => {
-  return fetch(`${MOVIE_API_BASE_URL}&i=${id}`)
-    .then(response => response.json())
-    .catch(error => error);
+const getMovie = async id => {
+  try {
+    const request = await fetch(`${MOVIE_API_BASE_URL}&i=${id}`);
+    const json = await request.json();
+    return json;
+  } catch (error) {
+    console.error('API:getMovie', error);
+  }
 };
 
 export { getMovies, getMovie };
